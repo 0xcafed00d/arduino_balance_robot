3d0d88aa9e9b228a12dea8a7d636962be2714939
Lee Witek <neomech@gmail.com>
2015-12-24 23:48:29 +0000

    code formating

--- src/motordrive.h
+++ src/motordrive.h
@@ -4,25 +4,25 @@
 #include <Servo.h>
 
 struct ServoConfig {
-  int zeropoint;
-  int range;
+	int zeropoint;
+	int range;
 };
 
 class MotorDrive {
-   private:
-  ServoConfig m_lconf;
-  ServoConfig m_rconf;
-  Servo* m_lservo;
-  Servo* m_rservo;
+  private:
+	ServoConfig m_lconf;
+	ServoConfig m_rconf;
+	Servo* m_lservo;
+	Servo* m_rservo;
 
-   public:
-  MotorDrive(Servo* lservo, Servo* rservo);
-  void setServoConfig(const ServoConfig& lconf, const ServoConfig& rconf);
+  public:
+	MotorDrive(Servo* lservo, Servo* rservo);
+	void setServoConfig(const ServoConfig& lconf, const ServoConfig& rconf);
 
-  void drive(int value);
-  void drive(int lvalue, int rvalue);
+	void drive(int value);
+	void drive(int lvalue, int rvalue);
 
-  void setRaw(int lvalue, int rvalue);
+	void setRaw(int lvalue, int rvalue);
 };
 
 #endif
